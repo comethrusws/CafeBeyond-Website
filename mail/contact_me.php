ebefe66d78b77ba14c3f1ca9156d556d547bb6a9
chillobae <basabjha@naver.com>
2023-08-10 21:43:02 +0545

    Commit #4

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,7 +13,7 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
-$to = 'basabjha.13@gmail.com'; 
+$to = 'jhasachit@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: support@cafebeyond.com\n"; 
